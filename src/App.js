e834d6dbe82527b2ca2133ac37bcd34237bbfb5f
Andreas Feuerstein <andreas.feuerstein@mail.de>
2021-09-14 12:23:15 +0200

    Replaced BrowserRouter with HashRouter for GH-Pages support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Layout from './layout/Layout';
 
 export default function App() {
+
     return (
         <React.StrictMode>
-            <BrowserRouter>
+            <HashRouter>
                 <Layout />
-            </BrowserRouter>
+            </HashRouter>
         </React.StrictMode>
     );
 }
